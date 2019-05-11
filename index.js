78be2879a306c5787c5d988a4acfdfff320a5d88
jonathonkindle <jonathonkindle@gmail.com>
2019-05-10 23:20:32 -0700

    changed routing for html pages

--- index.js
+++ index.js
@@ -1,24 +1,45 @@
-var http = require("http");
-var fs = require("fs");
+var express = require('express');
+var app = express();
 
-var server = http.createServer(function(req, res) {
-    console.log('A request has been made at: ' + req.url);
-    if(req.url === '/' || req.url ==='/home'){
-        res.writeHead(200, {"Content-Type": "text/html"});
-        fs.createReadStream(__dirname + '/public/home.html').pipe(res);
-    } else if(req.url === '/about'){
-        res.writeHead(200, {"Content-Type": "text/html"});
-        fs.createReadStream(__dirname + '/public/about.html').pipe(res);
-    } else if(req.url === '/api'){
-        var api = [{name: 'jonathon', class: 'itc230'}];
-        res.writeHead(200, {"Content-Type": "application.json"});
-        res.end(JSON.stringify(api));
-    } else{
-        res.writeHead(404, {"Content-Type": "text/html"});
-        fs.createReadStream(__dirname + '/public/404.html').pipe(res);
-    }
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/public/home.html');
 });
 
-server.listen(3000);
+app.get('/about', function(req, res){
+    res.sendFile(__dirname + '/public/about.html');
+});
 
-console.log("Server is operating on port 3000");
+app.get('/contact', function(req, res){
+    res.sendFile(__dirname + '/public/contact.html');
+});
+
+app.listen(3000);
+
+
+// the above code performs the same thing as the code below however it uses express to do so
+
+
+// var http = require("http");
+// var fs = require("fs");
+
+// var server = http.createServer(function(req, res) {
+//     console.log('A request has been made at: ' + req.url);
+//     if(req.url === '/' || req.url ==='/home'){
+//         res.writeHead(200, {"Content-Type": "text/html"});
+//         fs.createReadStream(__dirname + '/public/home.html').pipe(res);
+//     } else if(req.url === '/about'){
+//         res.writeHead(200, {"Content-Type": "text/html"});
+//         fs.createReadStream(__dirname + '/public/about.html').pipe(res);
+//     } else if(req.url === '/api'){
+//         var api = [{name: 'jonathon', class: 'itc230'}];
+//         res.writeHead(200, {"Content-Type": "application.json"});
+//         res.end(JSON.stringify(api));
+//     } else{
+//         res.writeHead(404, {"Content-Type": "text/html"});
+//         fs.createReadStream(__dirname + '/public/404.html').pipe(res);
+//     }
+// });
+
+// server.listen(3000);
+
+// console.log("Server is operating on port 3000");
