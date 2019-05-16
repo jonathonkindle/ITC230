4f848659d66ba56a0ec53a501d7834f85935c117
jonathonkindle <jonathonkindle@gmail.com>
2019-05-15 21:49:50 -0700

    added data to books.js

--- books.js
+++ books.js
@@ -1,27 +1,35 @@
 var books = [
-    {title: '', author: '', price: ''},
-    {title: '', author: '', price: ''},
-    {title: '', author: '', price: ''},
-    {title: '', author: '', price: ''},
-    {title: '', author: '', price: ''}
+    {title: 'Effective Java: Third Edition', author: 'Joshua Bloch', price: 43.86},
+    {title: 'Eloquent Javascript: Third Edition', author: 'Marijn Haverbeke', price: 25.31},
+    {title: 'Web Developement with MongoDB and Node: Third Edition', author: 'Bruno Joseph D\'mello', price: 39.99},
+    {title: 'JavaScript, The Good Parts', author: 'Douglas Crockford', price: 15.52},
+    {title: 'Learning Python: 5th Edition', author: 'Mark Lutz', price: 21.96}
 ];
 
-//ES6 Syntax
-const getAll = () => {
+module.exports = (book) => {
+
+    book.getAll (){
         return books
     };
+};
 
-    someFunction = (someParam) => {
 
-    }
+//ES6 Syntax
+// const getAll = () => {
+//         return books
+//     };
 
-//ES5 Syntax
-// function getAll() {
+//     someFunction = (someParam) => {
 
+//     }
+
+// //ES5 Syntax
+// // function getAll() {
+
+// // }
+
+// module.exports = {
+//     getAll
+// //     someFunction,
+// //     books
 // }
-
-module.exports = {
-    getAll
-//     someFunction,
-//     books
-}
