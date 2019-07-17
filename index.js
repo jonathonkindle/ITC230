ec6c11ac8ba7cf539ceeafded2f4a8c6fb02a787
jonathon <jonathonkindle@gmail.com>
2019-07-16 18:11:46 -0700

    starting the transition to express handlebars

--- index.js
+++ index.js
@@ -1,51 +1,61 @@
 const express = require('express');
 const app = express();
-const books = require('./books');
+const exphb = require('express-handlebars');
+// const books = require('./books');
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/public/home.html');
+app.engine('handlebars', exphb({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/public/home.html');
+// });
+
+app.get('/', (req, res) => {
+    res.render('index');
 });
 
-app.get('/about', function(req, res){
-    res.sendFile(__dirname + '/public/about.html');
-});
+// app.get('/about', (req, res) => {
+//     res.sendFile(__dirname + '/public/about.html');
+// });
 
-app.get('/contact', function(req, res){
-    res.sendFile(__dirname + '/public/contact.html');
-});
+// app.get('/contact', (req, res) => {
+//     res.sendFile(__dirname + '/public/contact.html');
+// });
 
-app.get('/get', function(req, res){
-    let found = books.get(req.query.title)
-    if(found){
-        res.send(JSON.stringify(found))
-    } else{
-        res.send("Book not available")
-    };
-});
+// app.get('/get', (req, res) => {
+//     let found = books.get(req.query.title)
+//     if(found){
+//         res.send(JSON.stringify(found))
+//     } else{
+//         res.send("Book not available")
+//     };
+// });
 
-app.get('/getAll', function(req, res){
-    let found = books.getAll()
-    if(found){
-        res.send(JSON.stringify(found, null, 2))
-    } else{
-        res.send("no books available")
-    };
-});
+// app.get('/getAll', (req, res) => {
+//     let found = books.getAll()
+//     if(found){
+//         res.send(JSON.stringify(found, null, 2))
+//     } else{
+//         res.send("no books available")
+//     };
+// });
 
-app.get('/deleteItem', function(req, res){
-    let result = books.deleteItem(req.query.title)
-    if(result.deleted){
-        res.send(req.query.title + " has been deleted")
-    } else{
-        res.send(req.query.title + " not found")
-    };
-});
+// app.get('/deleteItem', (req, res) => {
+//     let result = books.deleteItem(req.query.title)
+//     if(result.deleted){
+//         res.send(req.query.title + " has been deleted")
+//     } else{
+//         res.send(req.query.title + " not found")
+//     };
+// });
 
-app.get('/add', function(req, res){
+// app.get('/add', (req, res) => {
     
-});
+// });
 
-app.listen(3000);
+app.listen(3000, () => {
+    console.log("Your server has started at port ", 3000);
+});
 
 
 // the above code performs the same thing as the code below however it uses express to do so
