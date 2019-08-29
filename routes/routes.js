c92c69d8a4d911f70e1b04ecc617f565694c814e
jonathonkindle <jonathonkindle@gmail.com>
2019-08-28 17:38:51 -0700

    cleaning up current files

--- routes/routes.js
+++ routes/routes.js
@@ -20,7 +20,6 @@ router.get('/books', (req, res, next) => {
 // api route for getting all books from database
 router.get('/api/books', (req, res, next) => {
     Book.find().then((books) => {
-        // res.json(books);
         res.json(books);  
     });
 });
